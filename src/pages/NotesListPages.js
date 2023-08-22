91091c52102887c4029f0e80147b0ec8166fe733
vinubhat092 <swapnil.sharma@revealhealthtech.com>
2023-08-22 11:16:13 +0530

    first commit

---
+++ src/pages/NotesListPages.js
@@ -0,0 +1,28 @@
+import React, {useState,useEffect} from 'react'
+import ListItem from '../components/ListItem'
+const NotesListPages = () => {
+    let [notes, setNotes] = useState([])
+
+    useEffect (()=>{
+        getNotes()
+    },[])
+
+    let getNotes = async ()=>{
+        console.log("asdfgfd")
+        let response = await fetch("http://127.0.0.1:8000/api/notes/")
+        let data = await response.json()
+        console.log("DATA:",data)
+        setNotes(data)
+    }
+  return (
+    <div>
+        <div className='notes-list'>
+            {notes.map((note,index) =>(
+                <ListItem key={index} note={note}/>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default NotesListPages
